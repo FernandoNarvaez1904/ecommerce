7732824741eb541fc5039c972c8da4c35a6f1bc7
Diego Romero <diego.romero@unleashtechnologies.net>
2023-11-17 17:14:52 -0600

    Basic product Scroll and Filtering

--- apps/expo/src/screens/home.tsx
+++ apps/expo/src/screens/home.tsx
@@ -1,16 +1,60 @@
-import React from "react";
-
-import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
-import { useAuth } from "@clerk/clerk-expo";
+import React, { useMemo, useState } from "react";
+import { FlatList, Text, View, Image, TextInput } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { FlashList } from "@shopify/flash-list";
-import type { inferProcedureOutput } from "@trpc/server";
-import type { AppRouter } from "@acme/api";
+import { trpc } from "../utils/trpc";
+import { inferProcedureOutput } from "@trpc/server";
+import { AppRouter } from "@acme/api";
 
-export const HomeScreen = () => {
+function HomeScreen() {
+  const { data } = trpc.item.all.useQuery();
+
+  const [nameFilter, setNameFilter] = useState("");
+
+  const filteredData = useMemo(
+    () =>
+      data?.filter((item) =>
+        item.name.toLowerCase().includes(nameFilter.toLocaleLowerCase()),
+      ) ?? [],
+    [nameFilter, data],
+  );
+
   return (
-    <SafeAreaView className="bg-[#2e026d] bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      <View className="h-full w-full p-4">Home Screen</View>
+    <SafeAreaView className="px-2">
+      <TextInput
+        className="my-2 rounded border py-1 pl-3"
+        placeholder="Busca un Producto"
+        onChangeText={setNameFilter}
+      />
+      <FlatList
+        data={filteredData}
+        renderItem={(item) => (
+          <View className="w-[49%]">
+            <ProductCard item={item.item} key={item.item.id} />
+          </View>
+        )}
+        ItemSeparatorComponent={() => <View className="h-2 w-[2%]" />}
+        numColumns={2}
+        columnWrapperStyle={{ justifyContent: "space-between" }}
+      />
     </SafeAreaView>
   );
-};
+}
+
+export interface ProductCardProps {
+  item: inferProcedureOutput<AppRouter["item"]["byId"]>;
+}
+
+function ProductCard({ item }: ProductCardProps) {
+  return (
+    <View className="rounded border border-gray-400 px-2 py-2">
+      <Image
+        source={{ uri: item.image_url ?? "" }}
+        className="h-16"
+        resizeMode="contain"
+      />
+      <Text numberOfLines={1}>{item.name}</Text>
+    </View>
+  );
+}
+
+export default HomeScreen;
