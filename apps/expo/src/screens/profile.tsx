e819bd280c085ee0f82a5e653fcfad2a0c417ad0
Diego Romero <diego.romero@unleashtechnologies.net>
2023-11-26 10:29:16 -0600

    Create AuthenticationCard

--- apps/expo/src/screens/profile.tsx
+++ apps/expo/src/screens/profile.tsx
@@ -1,31 +1,14 @@
 import { SignedIn, SignedOut, useAuth } from "@clerk/clerk-expo";
 import { Text, View, TouchableOpacity } from "react-native";
-
-import SignInForm from "../components/auth/SignInForm";
-import SignUpForm from "../components/auth/SignUpForm";
-import { useState } from "react";
+import AuthenticationCard from "../components/auth/AuthenticationCard";
 
 function ProfileScreen() {
   const { signOut } = useAuth();
-  const [needsNewAccount, setNeedsNewAccount] = useState(false);
 
   return (
     <View className="h-full justify-center px-10">
       <SignedOut>
-        <View className="rounded  border border-gray-100 bg-white p-5 shadow">
-          {needsNewAccount ? <SignUpForm /> : <SignInForm />}
-
-          <TouchableOpacity
-            className="mt-2.5"
-            onPress={() => setNeedsNewAccount((prev) => !prev)}
-          >
-            <Text className="self-center font-normal text-gray-500">
-              {needsNewAccount
-                ? "No Account? Sign Up"
-                : "Have an Account? Sign In"}
-            </Text>
-          </TouchableOpacity>
-        </View>
+        <AuthenticationCard />
       </SignedOut>
       <SignedIn>
         <TouchableOpacity onPress={() => signOut()}>
