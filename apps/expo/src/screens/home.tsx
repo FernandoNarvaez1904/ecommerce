185d67259b48a2ea1da9067553b22f84de7e8a0f
Diego Romero <diego.romero@unleashtechnologies.net>
2023-11-26 04:18:18 -0600

    Add icons to home tab

--- apps/expo/src/screens/home.tsx
+++ apps/expo/src/screens/home.tsx
@@ -4,16 +4,44 @@ import ProfileScreen from "./profile";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { HomeTabParamList } from "../types/navigation";
 import CartListScreen from "./cart-list";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator<HomeTabParamList>();
 
 function HomeScreen() {
   return (
     <SafeAreaView className="h-full">
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Search" component={ItemListScreen} />
-        <Tab.Screen name="Cart" component={CartListScreen} />
-        <Tab.Screen name="My Profile" component={ProfileScreen} />
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarShowLabel: false,
+        })}
+      >
+        <Tab.Screen
+          name="Search"
+          component={ItemListScreen}
+          options={{
+            tabBarIcon: (props) => (
+              <Ionicons name="search-outline" {...props} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Cart"
+          component={CartListScreen}
+          options={{
+            tabBarIcon: (props) => <Ionicons name="cart-outline" {...props} />,
+          }}
+        />
+        <Tab.Screen
+          name="My Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: (props) => (
+              <Ionicons name="person-outline" {...props} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </SafeAreaView>
   );
